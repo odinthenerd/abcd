b5319237d89349c2b606a4081919b43f7b0637ee
Odin Holmes <holmes.odin@gmail.com>
2017-08-26 18:02:24 +0200

    changed how abilities work

---
+++ src/kvasir/abcd/abilities.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <type_traits>
+#include <kvasir/mpl/mpl.hpp>
+namespace kvasir {
+    namespace abcd {
+        ///Use this to associate abilities with an agent without specializing traits
+        template<typename...Ts>
+        struct abilities {};
+
+        template<typename Agent, typename Capability>
+        struct has_ability : std::false_type {
+        };
+
+        template<template<typename...> class W, typename...Ts, typename...Us, typename Ability>
+        struct has_ability<W<abilities<Ts...>,Us...>,Ability> :
+                mpl::call<mpl::find_if<mpl::same_as<Ability>,mpl::always<mpl::true_>,mpl::always<mpl::false_>>>{};
+
+        ///require this capability to be called in two phase init
+        struct requires_init_and_destruct {
+        };
+    }
+}
---
+++ src/kvasir/abcd/optional.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "abilities.hpp"
+#include "types.hpp"
+#include "kvasir/mpl/mpl.hpp"
+
+namespace kvasir{
+    namespace abcd{
+		namespace detail{
+			using namespace mpl;
+			template<template<typename> class B, typename T, typename C, typename FT, typename FF>
+			void optional_impl(const bool_<true>,B<T> *base, ability_t<C> a, FT lt, FF lf){
+				lt(access<T>(base)[a]);
+			};
+			template<typename C, typename FT, typename FF>
+			void optional_impl(const bool_<false>,void*, ability_t<C>, FT, FF lf){
+				lf();
+			};
+			template<typename T, typename U>
+			constexpr call<unpack<find_if<push_back<T,cfe<has_ability>>,always<bool_<true>>,always<bool_<false>>>>,U> has_ability;
+		}
+ 		template<template<typename> class B, typename T, typename C, typename FT, typename FF>
+		void optional(B<T> *base, ability_t<C> a, FT lt, FF lf) {
+			optional_impl(detail::has_ability<C,T>, base, a, lt, lf);
+		}
+    }
+}
