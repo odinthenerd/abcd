952d771aa8837940c379527ed14ac1554214db45
hhggit <zh.xz@outlook.com>
2019-06-17 09:58:10 +0800

    allow const access

--- src/kvasir/abcd/access.hpp
+++ src/kvasir/abcd/access.hpp
@@ -38,6 +38,24 @@ namespace kvasir {
             void for_each(ability_t <C>, F l) {
                 detail::for_each_helper(*this, l, detail::filter_by_capability<T, C>{});
             }
+
+
+            template<typename U>
+            const U &operator[](index_t <U>) const {
+                return std::get<detail::index_from_tuple<U,typename T::data_type>::value>(static_cast<const T*>(this)->data);
+            }
+            template<typename U>
+            const auto &operator[](ability_t <U>) const {
+                return std::get<detail::index_from_ability<U,typename T::data_type>::value>(static_cast<const T*>(this)->data);
+            }
+            template<typename C, typename FT, typename FF>
+            void optional(ability_t<C> a, FT lt, FF lf) const {
+                detail::optional_impl(detail::ability_in_tuple<C,T>, *this, a, lt, lf);
+            }
+            template<typename C, typename F>
+            void for_each(ability_t <C>, F l) const {
+                detail::for_each_helper(*this, l, detail::filter_by_capability<T, C>{});
+            }
         };
 
         template<typename T>
@@ -45,8 +63,18 @@ namespace kvasir {
             return *self;
         }
 
+        template<typename T>
+        const access<T> &agents(const access<T> *self) {
+            return *self;
+        }
+
         template<typename T, typename C, typename F>
         void for_each(access<T> *self, ability_t<C> a, F l) {
+            self->for_each(a, l);
+        }
+
+        template<typename T, typename C, typename F>
+        void for_each(const access<T> *self, ability_t<C> a, F l) {
             self->for_each(a, l);
         }
     }
